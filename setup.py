2ce6fa43281bbd159ad0ab9f106f2b2cdd97af7d
Marijan Beg <marijan.beg@xfel.eu>
2018-06-30 00:48:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.rst") as f:
+with open("README.md", 'r+', encoding="utf-8") as f:
     readme = f.read()
 
 setuptools.setup(
@@ -9,7 +9,7 @@ setuptools.setup(
     description=("A metapackage that includes oommfc and Jupyter notebook"),
     long_description=readme,
     url='https://joommf.github.io',
-    author='Marijan Beg, Ryan A. Pepper, and Hans Fangohr',
+    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=["oommfc",
