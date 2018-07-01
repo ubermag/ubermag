77d7031094ec25c308bcbafcd7c77f7b91cb25df
Marijan Beg <marijan.beg@xfel.eu>
2018-07-01 13:49:26 +0200

    Update setup.py and update MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 import setuptools
 
-with open("README.md", 'r+', encoding="utf-8") as f:
-    readme = f.read()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name="joommf",
-    version="0.2",
-    description=("A metapackage that includes oommfc and Jupyter notebook"),
-    long_description=readme,
+    name='joommf',
+    version='0.2',
+    description='Meta package for JOOMMF project.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
-    install_requires=["oommfc",
-                      "jupyter",
-                      "pytest"],
+    install_requires=['oommfc',
+                      'jupyter'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
                  'Topic :: Scientific/Engineering :: Physics',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3 :: Only']
+                 'Natural Language :: English']
 )
