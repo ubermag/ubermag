4e3c88d6ffbbd822fc4af341056e0bf360d7b350
Marijan Beg <marijan.beg@xfel.eu>
2018-07-01 13:59:24 +0200

    Fix bug in __init__.py.

--- joommf/__init__.py
+++ joommf/__init__.py
@@ -3,7 +3,7 @@ import pkg_resources
 
 def test():
     import pytest  # pragma: no cover
-    return pytest.main(["-v", "--pyargs", "joommfutil"])  # pragma: no cover
+    return pytest.main(["-v", "--pyargs", "joommf"])  # pragma: no cover
 
 __version__ = pkg_resources.get_distribution(__name__).version
 __dependencies__ = pkg_resources.require(__name__)
