d5e7e9975f40389514a422168eb41ef8507ca744
Marijan Beg <marijan.beg@xfel.eu>
2018-07-02 12:27:25 +0200

    Close #84.

--- joommf/__init__.py
+++ joommf/__init__.py
@@ -1,13 +1,12 @@
 import pytest
 import pkg_resources
 
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
+
 
 def test():
     return pytest.main(["-v", "--pyargs", "joommfutil",
-                        #"discretisedfield",
-                        "oommfodt",
+                        "discretisedfield", "oommfodt",
                         "micromagneticmodel", "oommfc",
                         "joommf"])  # pragma: no cover
-
-__version__ = pkg_resources.get_distribution(__name__).version
-__dependencies__ = pkg_resources.require(__name__)
