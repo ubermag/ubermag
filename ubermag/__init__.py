25a15eee164af269fb5d8ac4c7da90c86d98674a
Marijan Beg <marijan.beg@xfel.eu>
2019-11-04 13:39:34 +0000

    Add docs for ubermag.test().

--- ubermag/__init__.py
+++ ubermag/__init__.py
@@ -6,6 +6,17 @@ __dependencies__ = pkg_resources.require(__name__)
 
 
 def test():
+    """Runs tests for Ubermag and all its dependencies.
+
+    The tests are going to be run for:
+      - ubermagutil
+      - discretisedfield
+      - ubermagtable
+      - micromagneticmodel
+      - oommfc
+      - ubermag
+
+    """
     return pytest.main(['-m', 'not travis and not docker',
                         '-v', '--pyargs', 'ubermagutil',
                         'discretisedfield', 'ubermagtable',
