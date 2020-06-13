27eebd8ebfefa106aa7aac679e47aed62f6d040a
Marijan Beg <m.beg@soton.ac.uk>
2020-06-13 22:32:27 +0100

    Add more tests.

--- ubermag/__init__.py
+++ ubermag/__init__.py
@@ -6,19 +6,7 @@ __dependencies__ = pkg_resources.require(__name__)
 
 
 def test():
-    """Runs tests for Ubermag and all its dependencies.
-
-    The tests are going to be run for:
-      - ubermagutil
-      - discretisedfield
-      - ubermagtable
-      - micromagneticmodel
-      - oommfc
-      - ubermag
-
-    """
     return pytest.main(['-m', 'not travis and not docker',
-                        '-v', '--pyargs', 'ubermagutil',
-                        'discretisedfield', 'ubermagtable',
-                        'micromagneticmodel', 'oommfc',
-                        'ubermag'])  # pragma: no cover
+                        '-v', '--pyargs', 'ubermagutil', 'discretisedfield',
+                        'ubermagtable', 'micromagneticmodel', 'oommfc',
+                        'micromagnetictests', 'ubermag'])  # pragma: no cover
