f4afaee51250106f9f7153f9d3781199bc8df9dd
Marijan Beg <m.beg@soton.ac.uk>
2020-06-13 23:06:08 +0100

    Fix tests.

--- ubermag/tests/test_init.py
+++ ubermag/tests/test_init.py
@@ -2,10 +2,10 @@ import ubermag
 
 
 def test_version():
-    assert isinstance(um.__version__, str)
+    assert isinstance(ubermag.__version__, str)
     assert '.' in ubermag.__version__
 
 
 def test_dependencies():
-    assert isinstance(um.__dependencies__, list)
+    assert isinstance(ubermag.__dependencies__, list)
     assert len(ubermag.__dependencies__) > 0
