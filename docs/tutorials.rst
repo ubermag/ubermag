2342629429c4910dd2fac581bcfdb339de42609f
Marijan Beg <m.beg@soton.ac.uk>
2020-06-15 14:46:48 +0100

    Restructure docs.

--- docs/tutorials.rst
+++ docs/tutorials.rst
@@ -4,12 +4,15 @@ Tutorials
 .. toctree::
    :maxdepth: 1
 
-   ipynb/00-tutorial-welcome
-   ipynb/01-tutorial-installation
-   ipynb/02-tutorial-testing
-   ipynb/03-tutorial-upgrade
-   ipynb/04-tutorial-micromagnetic-tutorials
-   ipynb/05-tutorial-ubermag-cloud
-   ipynb/06-tutorial-support
-   ipynb/07-tutorial-how-to-cite
-   ipynb/08-tutorial-acknowledgements
+   ipynb/what-is-ubermag
+   ipynb/installation-testing-update
+   ipynb/documentation
+   ipynb/ubermag-cloud
+   ipynb/other
+
+**Advanced**:
+
+.. toctree::
+   :maxdepth: 1
+
+   ipynb/advanced-installation
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='ubermag',
-    version='0.1.1',
+    version='0.2',
     description='Meta package for Ubermag project.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,7 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires='>=3.6',
-    install_requires=['oommfc==0.8.17'],
+    install_requires=['oommfc==0.8.17',
+                      'micromagneticdata==0.3'],
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Intended Audience :: Education',
                  'Intended Audience :: Developers',
