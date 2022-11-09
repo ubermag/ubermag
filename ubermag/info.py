524ab0d59e21d13ec27c0bf2b83d309e6467db15
Martin Lang <m.lang@soton.ac.uk>
2022-11-09 13:23:46 +0100

    Function to collect debug info.

---
+++ ubermag/info.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def debug_info():
+    """Collect inforamtion about ubermag subpackages and available calculators."""
+    info_str = f"Platform: {sys.platform}\nPython: {sys.version}\n\n"
+
+    for pkg in [
+        "ubermagutil",
+        "discretisedfield",
+        "ubermagtable",
+        "micromagneticmodel",
+        "micromagneticdata",
+        "micromagnetictests",
+        "mumax3c",
+        "oommfc",
+        "mag2exp",
+        "ubermag",
+    ]:
+        exec(f"import {pkg}")
+        version = eval(f"{pkg}.__version__")
+        info_str += f"{pkg}: {version}\n"
+
+    info_str += "\n"
+
+    for pkg, calculator in [("oommfc", "OOMMF"), ("mumax3c", "Mumax3")]:
+        try:
+            runner = eval(f"{pkg}.runner.runner")
+        except OSError as e:
+            info_str += f"{calculator}: {e}\n"
+        else:
+            info_str += f"{calculator}: {runner}, {runner.version}\n"
+
+    return info_str
